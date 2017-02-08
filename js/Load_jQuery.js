901da6b44601b5ff9dbdc2653a92c756646298e5
fengmiaosen <fengmiaosen@gmail.com>
2017-02-08 22:28:23 +0800

    ES Decorators 函数实现高阶组件封装

--- js/Load_jQuery.js
+++ js/Load_jQuery.js
@@ -4,7 +4,24 @@
 import React from 'react';
 import { LazilyLoadFactory } from '../lib/LazilyLoad';
 
-class Load_jQuery extends React.Component {
+// ES Decorators函数实现高阶组件封装
+// 效果等同于
+// export default LazilyLoadFactory(Load_jQuery, {
+//     $: () => require('jquery')(),
+// });
+// 参考 http://technologyadvice.github.io/es7-decorators-babel6/
+const LazilyLoadDecorator = (Component) => {
+
+    return LazilyLoadFactory(Component, {
+        $: () => require('jquery')(),
+    });
+};
+
+// ES Decorators语法
+// 需要依赖babel-plugin-transform-decorators-legacy
+// babel-loader配置使用plugins: ["transform-decorators-legacy"]
+@LazilyLoadDecorator
+export default class Load_jQuery extends React.Component {
 
     componentDidMount() {
         console.log('Load_jQuery props:', this.props);
@@ -20,7 +37,3 @@ class Load_jQuery extends React.Component {
     }
 
 };
-
-export default LazilyLoadFactory(Load_jQuery, {
-    $: () => require('jquery')(),
-});
--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,8 @@ module.exports = {
                 exclude: /(node_modules)/,
                 loader: 'babel',
                 query: {
-                    presets: ['es2015', 'react']
+                    presets: ['es2015','stage-1', 'react'],
+                    plugins: ["transform-decorators-legacy"]
                 }
             },
             {
